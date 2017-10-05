9ea69dfa7bf8337abb51a122da441e78baae071a
seannives <seann.ives@gmail.com>
2017-10-05 10:29:39 -0400

    times up

--- src/common/selectors/assignments.js
+++ src/common/selectors/assignments.js
@@ -1,2 +1,13 @@
 export const getAssignments = state => state.assignments;
 export const getAssignmentById = (state, assignmentId) => state.assignments[assignmentId];
+
+/* TODO:
+- get assignment completeness by assignment id
+- get grade by assignment id
+- get quiz (including questinos and existing student answers) by assignment id
+
+then...
+- student action to answer question
+- teacher action to grade question
+
+*/
